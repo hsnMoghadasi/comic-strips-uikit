abcf42c86f5fbdb1a5ed32970aa57534bc4655c4
hassan moghadasi <moghadasi7708@gmail.com>
2023-03-29 17:14:35 +0330

    update

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import Button from './component/atoms/button';
+export { Button };
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const pkg = require('./package.json');
+const path = require('path');
+const libraryName = pkg.name;
+
 module.exports = {
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'myUnflappableComponent.js',
+        filename: 'index.js',
         library: libraryName,
         libraryTarget: 'umd',
         publicPath: '/dist/',
@@ -9,26 +15,45 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: 'myUnflappableComponent.css',
+            filename: 'index.css',
         }),
     ],
     module: {
-        rules: [
-            {
-                test: /\.*css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                })
-            },
-        ]
+        rules: [{
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    fallback: "file-loader",
+                    name: "[name][md5:hash].[ext]",
+                    outputPath: 'assets/',
+                    publicPath: '/assets/'
+                }
+            }]
+        },
+        {
+            test: /\.*css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader']
+            })
+        },
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
     },
     externals: {},
     resolve: {
         alias: {
             'react': path.resolve(__dirname, './node_modules/react'),
             'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-        }
+            'assets': path.resolve(__dirname, './src/assets')
+        },
+        extensions: ['*', '.js', '.jsx']
     },
     externals: {
         // Don't bundle react or react-dom      
